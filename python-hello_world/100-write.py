9ffbf16b635fd5f3e4296ae1bd23e120fd1576ce
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-19 18:22:51 -0800

    refactor: print a new line after the string

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
 
+
 def print_to_stderr():
-    
-    sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19')
+
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
     exit(1)
 
 print_to_stderr()
