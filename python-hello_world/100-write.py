75c7ce65fe26c974d4c3166f943866458dedb6cf
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-19 17:57:00 -0800

    refactor: print to the stderror using write function from sys module

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import sys
 
-def print_to_stderr(*str):
+def print_to_stderr():
     
-    print(*str, file = sys.stderr)
-    
+    sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19')
     exit(1)
 
-print_to_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19")
+print_to_stderr()
