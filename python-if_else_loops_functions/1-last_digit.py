fca35decf28c562e6914e1119d087ac0391dee58
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-20 20:00:53 -0800

    refactor: correct output when parameter is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 
-ld = abs(number) % 10
+ld = number % 10
+
+if number < 0:
+    ld = abs(number) % 10
+    ld = ld * (-1)
 
 if ld > 5:
     print(f"Last digit of {number} is {ld} and is greater than 5")
