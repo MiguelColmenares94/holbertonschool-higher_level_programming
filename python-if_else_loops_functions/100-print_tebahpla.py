181ceec23adf600284416dedb7c0cf28b155da9f
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 22:29:54 -0800

    style: delete white spaces

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,10 +2,10 @@
 
 
 for tebahpla in range(122, 96, -1):
-    
+
     if tebahpla % 2 == 0:
         pass
     elif tebahpla % 2 == 1:
         tebahpla -= 32
-    
+
     print("{:c}".format(tebahpla), end="")
