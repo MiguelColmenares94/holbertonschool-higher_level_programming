4f4d65e3a50ac241fecb39fbf4b097a7f93632b5
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 22:12:05 -0800

    style: add a blank line and delete white space according to PEP8

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def fizzbuzz():
 
     for counter in range(1, 101):
         a = counter % 3
         b = counter % 5
-        
+
         if a == 0 and b == 0:
             print("FizzBuzz ", end="")
         elif a == 0:
