461c1839191d320f5eaf9c349ae6402b8a9edc5c
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 22:42:56 -0800

    feat: creates a copy of the string, removing the character at the position n

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def remove_char_at(str, n):
+
+    for i in range(len(str)):
+        if i == n:
+            pass
+        else:
+            print("{}".format(str[i]), end="")
