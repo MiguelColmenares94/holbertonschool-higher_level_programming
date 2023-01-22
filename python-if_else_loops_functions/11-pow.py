a6586c0aa3074cbd472e64b8ad8e3776d2771aee
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 21:06:38 -0800

    feat: computes a to the power of b and return the value.

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def add(a, b):
+
+    # Add validations just for fun and learn, exercise expect error
+    checkA = isinstance(a, int)
+    checkB = isinstance(b, int)
+
+    if (checkA and checkB) == 1:
+        return(a ** b)
+    else:
+        return(a ** b)
