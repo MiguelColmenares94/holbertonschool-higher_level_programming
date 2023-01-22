638da5010a679733af3a947b27f1affc2f03c2c8
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 18:19:04 -0800

    feat: prints all possible different combinations of two digits.

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for a in range(1, 10):
+        if i == a or i == 9:
+            a += 1
+        elif i >= a:
+            a += 1
+        else:
+            print("{}".format(i), end="")
+            print("{}, ".format(a), end="")
+
+print("89")
