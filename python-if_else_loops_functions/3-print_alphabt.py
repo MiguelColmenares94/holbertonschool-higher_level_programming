b786d303f28a0ed14a341100d5814e18526b2814
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 17:32:22 -0800

    refactor: only use one loop in script

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 101):
-    print("{:c}".format(letter), end="")
-
-for letter in range(102, 113):
-    print("{:c}".format(letter), end="")
-
-for letter in range(114, 123):
-    print("{:c}".format(letter), end="")
+for letter in range(97, 123):
+    if letter != 101 and letter != 113:
+        print("{:c}".format(letter), end="")
