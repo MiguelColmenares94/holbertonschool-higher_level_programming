2cffb2a4a111c041cdfd806a56e83547bafd0b3c
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 19:29:02 -0800

    refactor: correct range for loop

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@
 
 def uppercase(str):
 
-    for i in str:
-        if i != "\0":
-            print("{:c}".format((ord(str[i] - 32))), end="")
-        else:
-            print("\n")
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            j = ord(str[i]) - 32
+            print("{:c}".format(j), end="")
+    print("\n")
