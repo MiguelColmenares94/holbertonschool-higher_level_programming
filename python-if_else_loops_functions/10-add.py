0858a11aa5094e8cd4e992e61484c614a1a933ac
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 20:49:08 -0800

    feat: set error return according to exercise output

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -3,10 +3,11 @@
 
 def add(a, b):
 
+    #Add validations just for fun and learn, exercise expect error
     checkA = isinstance(a, int)
     checkB = isinstance(b, int)
 
     if (checkA and checkB) == 1:
         return(a + b)
     else:
-        pass
+        return(a + b)
