ef2137d8d6be983ebcdf4fae3b3d1dbe17722422
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 19:39:40 -0800

    refactor: add case where input is different of lowercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,5 +6,7 @@ def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             j = ord(str[i]) - 32
+        else:
+            j = i
             print("{:c}".format(j), end="")
-    print("\n")
+    print()
