03ce98ee9175e18058e8d2c117f6cab9ffbe628d
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 20:40:34 -0800

    feat: adds two integers and returns the result.

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def add(a, b):
+
+    checkA = isinstance(a, int)
+    checkB = isinstance(b, int)
+
+    if (checkA and checkB) == 1:
+        result = a + b
+    else:
+        pass
