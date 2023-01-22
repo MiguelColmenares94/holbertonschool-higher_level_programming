afb3826c8568e07847ecad0a3fcd16ea1afc0da9
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 19:54:02 -0800

    refactor: add missing parenthesis

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -7,6 +7,6 @@ def uppercase(str):
         j = ord(str[i])
 
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            j = ord(str[i] - 32)
+            j = ord(str[i]) - 32
         print("{:c}".format(j), end="")
     print()
