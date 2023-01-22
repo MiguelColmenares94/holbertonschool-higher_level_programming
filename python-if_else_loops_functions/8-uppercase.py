4aaafc5db1fd3248508c8340026f56265f2b3893
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-21 19:51:25 -0800

    refactor: fix variable scope within function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,10 +4,9 @@
 def uppercase(str):
 
     for i in range(len(str)):
+        j = ord(str[i])
+
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            j = ord(str[i]) - 32
-        else:
-            j = ord(i)
-    
-    print("{:c}".format(j), end="")
+            j = ord(str[i] - 32)
+        print("{:c}".format(j), end="")
     print()
