a33c446752832af0839765020c7db1e804b1e3f3
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-22 19:15:22 -0800

    feat: Build my own calculator!

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import calculator_1
+    from sys import argv
+    from os import exit
+
+    a = int(argv[1])
+    b = int(argv[3])
+    c = argv[2]
+    mod = calculator_1
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if c == "+":
+        print("{:d} {:c} {:d} = {:d}".format(a, c, b, mod.add(a, b)))
+        exit(0)
+    elif c == "-":
+        print("{:d} {:c} {:d} = {:d}".format(a, c, b, mod.sub(a, b)))
+        exit(0)
+    elif c == "*":
+        print("{:d} {:c} {:d} = {:d}".format(a, c, b, mod.mul(a, b)))
+        exit(0)
+    elif c == "/":
+        print("{:d} {:c} {:d} = {:d}".format(a, c, b, mod.div(a, b)))
+        exit(0)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
