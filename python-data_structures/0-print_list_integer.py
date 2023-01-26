f8105cad1a51b7c2e94136902a60541f74da278e
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 12:47:12 -0800

    fix: delete guard because function it's not called from a module

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 
-    def print_list_integer(my_list=[]):
-        """prints all integers of a list.
-        """
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+
+def print_list_integer(my_list=[]):
+    """prints all integers of a list.
+    """
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
