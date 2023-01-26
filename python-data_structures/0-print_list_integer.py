93efcf2f2e294df50b84fe5a66b48da6d875dc4c
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 12:39:06 -0800

    feat: initial commit

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__"
+
+
+def print_list_integer(my_list=[]):
+    """
+     prints all integers of a list.
+    """
+
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
