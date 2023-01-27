aa4c332b277350618a3d10804b6b40240723f298
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 17:31:58 -0800

    refact: validation in case list is empty

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,8 +4,8 @@
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list, in reverse order.
     """
-
-    my_list.reverse()
+    if my_list != "\0":
+        my_list.reverse()
 
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
