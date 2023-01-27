0720bd653e81704f21e285b82a0165560ed33c0a
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 17:16:19 -0800

    refact: return list with replaced elements as exercise ask

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -11,4 +11,5 @@ def replace_in_list(my_list, idx, element):
         return (my_list)
     else:
         del my_list[idx]
-        return (my_list.insert(idx, element))
+        my_list.insert(idx, element)
+        return (my_list)
