0c1ead0159eb0231b293d764a1e7385d52cdae10
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 16:35:57 -0800

    refact: function just return value, de, dont print anymore

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -7,8 +7,7 @@ def element_at(my_list, idx):
 
     if idx < 0:
         return None
-
-    if idx > len(my_list):
+    elif idx > len(my_list):
         return None
-
-    print("Element at index {} is {}".format(idx, my_list[idx]))
+    else:
+        return(idx)
