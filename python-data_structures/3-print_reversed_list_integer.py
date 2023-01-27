10e29d4e62fad591548471d3986e8141ad9b61b1
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 17:48:54 -0800

    refactor: correct output in case list type is None

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -6,9 +6,7 @@ def print_reversed_list_integer(my_list=[]):
     """
     if my_list is None:
         print(my_list)
-        return
     else:
         my_list.reverse()
-
-    for i in range(len(my_list)):
+        for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
