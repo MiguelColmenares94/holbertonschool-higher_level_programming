a58bf53a27e01ad49662c9adfefc5cef7a7636dd
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 18:54:39 -0800

    fix: correct function definition

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,9 +4,6 @@
 def no_c(my_string):
     """removes all characters c and C from a string.
     """
-
-    def no_c(my_string):
-
         for i in range(len(my_string)):
             if my_string[i] == chr(99) or my_string[i] == chr(67):
                 del my_string[i]
