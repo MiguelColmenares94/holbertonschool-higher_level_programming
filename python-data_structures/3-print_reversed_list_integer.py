834f4fb677a0e00e51c74bd4f0b89ee32c6cb082
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 17:29:42 -0800

    fix: correct function definition

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,8 +5,7 @@ def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list, in reverse order.
     """
 
-    def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
 
-        my_list.reverse()
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
