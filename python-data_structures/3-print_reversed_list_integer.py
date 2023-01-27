f511a84be70c898e807b1b42633de7bb1ccb4ba3
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 17:55:43 -0800

    refactor: return empty list if list is None

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,7 @@ def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list, in reverse order.
     """
     if my_list is None:
-        print(my_list)
+        return []
     else:
         my_list.reverse()
         for i in range(len(my_list)):
