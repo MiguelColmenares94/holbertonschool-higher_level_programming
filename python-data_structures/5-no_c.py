76382450b9c092fe9dac9223dc82bb24bacca71b
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 18:56:00 -0800

    style: fix indentation

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,8 +4,8 @@
 def no_c(my_string):
     """removes all characters c and C from a string.
     """
-        for i in range(len(my_string)):
-            if my_string[i] == chr(99) or my_string[i] == chr(67):
-                del my_string[i]
+    for i in range(len(my_string)):
+        if my_string[i] == chr(99) or my_string[i] == chr(67):
+            del my_string[i]
 
     return (my_string)
