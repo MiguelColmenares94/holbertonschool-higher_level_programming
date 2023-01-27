6b91d16a590c57dadce5251fe4320f3b0c9b6a27
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 18:24:51 -0800

    feat: replaces an element in a list at a specific position without modifying the original list (like in C)

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    """replaces an element in a list at a specific
+       position without modifying the original list (like in C)
+    """
+
+    new_list = my_list.copy()
+
+    if idx < 0:
+        return (new_list)
+    elif idx > (len(new_list) - 1):
+        return (new_list)
+    else:
+        del new_list[idx]
+        new_list.insert(idx, element)
+        return (new_list)
