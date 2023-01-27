69c8e40c42322d78a2ee387d9a3d002fa9c74edb
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 19:01:41 -0800

    refactor: create copy string to work on

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,8 +4,10 @@
 def no_c(my_string):
     """removes all characters c and C from a string.
     """
-    for i in range(len(my_string)):
-        if my_string[i] == chr(99) or my_string[i] == chr(67):
-            del my_string[i]
 
-    return (my_string)
+    copy = my_string.copy()
+    for i in range(len(copy)):
+        if copy[i] == chr(99) or copy[i] == chr(67):
+            copy.remove(i)
+
+    return (copy)
