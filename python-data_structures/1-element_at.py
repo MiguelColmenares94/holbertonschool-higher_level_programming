cd7806635b5636695b5b45d56f8a714726de398e
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-26 16:27:54 -0800

    fix: return value set to None because there are other possible return values from func

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,11 +2,13 @@
 
 
 def element_at(my_list, idx):
+    """retrieves an element from a list like in C
+    """
 
     if idx < 0:
-        return
+        return None
 
     if idx > len(my_list):
-        return
+        return None
 
     print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
