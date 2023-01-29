e8bbb0f347269344b35ff71bb03f25cf15f348de
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-28 21:10:26 -0800

    feat: prints a matrix of integers.

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """prints a matrix of integers.
+    """
+
+    for row in matrix:
+        for val in row:
+            print(val, end="")
+        print()
