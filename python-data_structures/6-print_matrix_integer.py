42dcc8446f0431b6c05d8f50f4588daf17998541
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-28 21:27:39 -0800

    refactor: fix range for correct print of values

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,9 +5,9 @@ def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers.
     """
 
-    for row in matrix:
-        for val in row:
-            print("{:d}".format(val), end="")
+    for row in range(len(matrix)):
+        for val in matrix[row]:
+            print("{:d}".format(matrix[row][val]), end="")
             if val != (len(matrix) - 1):
                 print(" ", end="")
 
