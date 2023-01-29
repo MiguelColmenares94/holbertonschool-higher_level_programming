cc68b9a857181816a4213d5d1b1fc45992653c0e
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-28 21:29:56 -0800

    fix: set correct variables to search for values

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,10 +5,10 @@ def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers.
     """
 
-    for row in range(len(matrix)):
-        for val in matrix[row]:
-            print("{:d}".format(matrix[row][val]), end="")
-            if val != (len(matrix) - 1):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
                 print(" ", end="")
 
         print()
