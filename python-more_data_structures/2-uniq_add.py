81db261b3c9d9e934ed79d428b4d2418fa2b6fa6
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-29 17:22:49 -0800

    feat: adds all unique integers in a list (only once for each integer)

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    """adds all unique integers in a list
+    (only once for each integer)
+    """
+
+    non_rep = set(my_list)
+    n = 0
+
+    for i in non_rep:
+        num += i
+
+    return (num)
