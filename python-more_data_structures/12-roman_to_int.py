dc49d8914e17b90f960c0c8cecaf2934aa3d5b3d
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-29 18:54:56 -0800

    refactor: convert input string into a list for correct analysis

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,10 +10,12 @@ def roman_to_int(roman_string):
 
     rom_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
+    string_to_l = []
+    string_to_l[:0] = roman_string
     converted_list = []
     rom_list = list(rom_num)
-    for ch in range(int(roman_string)):
-        if roman_string[i] == rom_list[i]:
+    for i in string_to_l:
+        if string_to_l[i] == rom_list[i]:
             converted_list.append(rom_num[i])
 
     return (sum(converted_list))
