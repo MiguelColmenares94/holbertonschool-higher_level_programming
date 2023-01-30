528b4d6b5236522246984100b7522791ac1c8556
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-29 17:13:46 -0800

    feat: replaces all occurrences of an element by another in a new list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    """replaces all occurrences of an element
+    by another in a new list
+    """
+
+    n_list = list(map(lambda x: replace if x == search else x, my_list))
+
+    return (new_list)
