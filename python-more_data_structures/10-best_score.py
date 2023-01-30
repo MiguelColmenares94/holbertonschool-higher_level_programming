82c5bff0657955bbc9463abb30b7a857253969b8
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-29 18:15:30 -0800

    fix: typo in var name causing not defined error

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     """returns a key with the biggest integer value
     """
 
-    if not a_dictonary:
+    if not a_dictionary:
         return (None)
 
     return(max(a_dictionary, key=a_dictionary.get))
