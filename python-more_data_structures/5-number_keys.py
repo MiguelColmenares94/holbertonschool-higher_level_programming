8c5b44e611325e59a6556ee488e669f69b664a57
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-29 17:35:49 -0800

    feat: returns the number of keys in a dictionary

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """returns the number of keys in a dictionary
+    """
+
+    dic_list = list(a_dictionary)
+    keys = (len(a_dictionary) - 1)
+    return (keys)
