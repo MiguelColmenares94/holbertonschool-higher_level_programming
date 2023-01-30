4afa1f76c05d8fa8ca354779a243c295e22e0216
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-29 19:17:30 -0800

    feat: prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """prints x elements of a list
+    """
+
+    count = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except:
+            break
+        else:
+            count += 1
+
+    print()
+    return (count)
