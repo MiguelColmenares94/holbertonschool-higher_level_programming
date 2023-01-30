6001c34b3828e7b1f1d642cb337edbc85de39fc3
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-29 17:52:55 -0800

    refactor: print uppercase before lowercase

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,6 +5,8 @@ def print_sorted_dictionary(a_dictionary):
     """prints a dictionary by ordered keys
     """
 
-    sorted(a_dictionary)
-    for i in a_dictionary:
+    dic_to_list = list(a_dictionary.keys())
+    dic_to_list.sort()
+
+    for i in dic_to_list:
         print("{}: {}".format(i, a_dictionary.get(i)))
