5ae5e2ca14d2b7c1f3850880697abbc79b045edf
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-01-29 17:43:48 -0800

    feat: prints a dictionary by ordered keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """prints a dictionary by ordered keys
+    """
+
+    sorted(a_dictionary)
+    for i in a_dictionary:
+        print("{}: {}".format(i, a_dictionary.get(i)))
