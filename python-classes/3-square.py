207e8e0c2cca2d615b8d01b49ef7177a0bebc96c
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-05 08:49:04 -0800

    style: adjust indentation for test

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,21 +2,21 @@
 """A class Square that defines a square by it's size"""
 
 
-class Square:
-    """Defines a square class with some conditions"""
-    def __init__(self, size=0):
-        """Method to initialize objects
+    class Square:
+        """Defines a square class with some conditions"""
+        def __init__(self, size=0):
+            """Method to initialize objects
             
-        Args:
-            size (int): size of the square
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+            Args:
+                size (int): size of the square
+            """
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
 
-    def area(self):
-        """Return: the current square area."""
-        return (area=self.__size ** 2)
+        def area(self):
+            """Return: the current square area."""
+            return (area=self.__size ** 2)
