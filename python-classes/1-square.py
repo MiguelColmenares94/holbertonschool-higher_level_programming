abad67d1026c09e1ede10b55fb914b41d8fa60dd
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-05 06:22:02 -0800

    feat: a class Square that defines a square by: (based on 0-square.py)

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a class Square"""
+
+
+class Square:
+    """Defines a square
+    """
+    def __init__(self, name="None", size="None"):
+        """Initializacion of objects
+
+        Args:
+            name(str): name of the object.
+            size(int): size of the square.
+        """
+        self.name = __name
+        self.size = __size
