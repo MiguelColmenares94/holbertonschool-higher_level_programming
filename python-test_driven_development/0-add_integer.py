cbabc598bc4547feef65ea0d88d3c650c8f08e5d
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-06 17:40:58 -0800

    feat: initial commit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Contains a method that returns the add of two int
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Return a + b as int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return (int(a) + int(b))
