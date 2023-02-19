1883b2358513d19e58513c84a59a61a880695eee
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-19 09:38:17 -0800

    style: delete extra whitespaces according to pep8

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,5 +26,5 @@ class Base:
         - Return: string of dictionary in json format"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-            string = json.dumps(list_dictionaries)
-            return string
+
+        return (json.dumps(list_dictionaries))
