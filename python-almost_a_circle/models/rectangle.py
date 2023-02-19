b4a2524da5a9b290fa83363dab7da5f2ae5faa9a
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-19 06:22:38 -0800

    refactor: set correct return value for __str__ method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -111,5 +111,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """Human readable string for understanding Rectangle instance"""
-        return print("[Rectangle] ({}) {}/{} - {}/{}"
-                     .format(self.id, self.x, self.y, self.width, self.height))
+        string = "[Rectangle] ({}) {}/{} - {}/{}"
+        string = string.format(self.id, self.x, self.y, self.width,
+                               self.height)
+        return string
