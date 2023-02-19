a65a7866f69bef5758eba58a7ce7312d8a63347c
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-18 21:57:07 -0800

    refactor: change if for elif for better performance

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,8 +38,7 @@ class Rectangle(Base):
         """
         if (type(new_value) is not int):
             raise TypeError("width must be an integer")
-
-        if (new_value <= 0):
+        elif (new_value <= 0):
             raise ValueError("width must be > 0")
         self.__width = new_value
 
@@ -57,8 +56,7 @@ class Rectangle(Base):
         """
         if (type(new_value) is not int):
             raise TypeError("height must be an integer")
-
-        if (new_value <= 0):
+        elif (new_value <= 0):
             raise ValueError("height must be > 0")
         self.__height = new_value
 
