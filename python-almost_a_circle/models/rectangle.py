c6210d18ecf9bc96aa3bb6cc55fe1a4da6274414
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-18 21:05:28 -0800

    refactor: set correct parameters for getter and setters

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,42 +11,81 @@ Class Rectangle(Base):
     Rectangle class, this class inherits from Base
     """
 
-    def def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__
-        super().id
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Rectangle's constructor
+
+        Args:
+            -width(int)
+            -height(int)
+            -x(int)
+            -y(int)
+            -id(int)
+        """
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
 
     @property
-    def width():
-        return = self.__width
+    def width(self):
+        """
+        Getter for private attribute width
+        """
+        return (self.__width)
 
     @width.setter
-    def width():
-        self.__width = width
+    def width(self, new_value):
+        """
+        Setter for private attribute width
+        Args:
+            -new_value(int)
+        """
+        self.__width = new_value
 
     @property
-    def height():
-        return = self.__height
+    def height(self):
+        """
+        Getter for private attribute height
+        """
+        return self.__height
 
     @height.setter
-    def height():
-        self.__height = height
+    def height(self, new_value):
+        """
+        Setter for private attribute height
+        Args:
+            -new_value(int)
+        """
+        self.__height = new_value
 
     @property
-    def x():
-        return = self.__x
+    def x(self):
+        """
+        Getter for private attribute x
+        """
+        return self.__x
 
     @x.setter
-    def x():
+    def x(self, new_value):
+        """
+        Setter for private attribute x
+        Args:
+            -new_value(int)
+        """
         self.__x = x
 
     @property
-    def y():
-        return = self.__y
+    def y(self):
+        """
+        Getter for private attribute y
+        """
+        return self.__y
 
     @y.setter
-    def y():
+    def y(self, new_value):
+        """
+        Setter for private attribute y
+        """
         self.__y = y
