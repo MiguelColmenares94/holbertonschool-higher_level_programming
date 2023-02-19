6ada601d5f4fae3eb30d1c4e9c407d576680db8c
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-18 20:46:03 -0800

    feat: create the class Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This module contains the class Rectangle that inherits from Base
+"""
+
+from base import Base
+
+
+Class Rectangle(Base):
+    def def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__
+        super().id
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width():
+        return = self.__width
+
+    @width.setter
+    def width():
+        self.__width = width
+
+    @property
+    def height():
+        return = self.__height
+
+    @height.setter
+    def height():
+        self.__height = height
+    
+    @property
+    def x():
+        return = self.__x
+
+    @x.setter
+    def x():
+        self.__x = x
+
+    @property
+    def y():
+        return = self.__y
+
+    @y.setter
+    def y():
+        self.__y = y
