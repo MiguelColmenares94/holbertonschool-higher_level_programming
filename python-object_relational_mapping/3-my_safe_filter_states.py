470eec0b973cdd94926db9551be3950067cb6ed8
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-03-19 19:37:42 -0700

    fix: add SQL injection protection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa the safe way"""
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument safe from SQL injection
+"""
 
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb as sql
+    import re
 
-    if (len(argv) != 4):
-        print('Trying to make a SQL Injection? just use: username,
-              password, database name')
+    if (len(argv) != 5):
+        print('Trying to make a SQL Injection? just use:
+               username, password, database name, state name')
+        exit(1)
+
+    querry = ' '.join(argv[4].split())
+
+    if (re.search('^[a-zA-Z ]+$', querry) is None):
+        print('Enter a valid name state (example: Nevada)')
         exit(1)
 
     try:
         db = sql.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                           passwd=argv[2], db=argv[3])
     except Exception:
-        print("Can not connect to the database")
+        print('Can not connect to the database')
         exit(0)
 
     cursor = db.cursor()
 
-    cursor.execute("""SELECT ct.id, ct.name, ct.name FROM cities AS ct
-                      INNER JOIN states AS st
-                      ON ct.state = st.id
-                      ORDER BY ct.id ASC;""")
+    cursor.execute("SELECT * FROM states \
+                    WHERE name = '{:s}' ORDER BY id ASC;".format(searched))
 
     result_query = cursor.fetchall()
 
