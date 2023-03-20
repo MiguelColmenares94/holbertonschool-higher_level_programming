cc715875bc503eec1150d3784a0b2b8f3b44bd8f
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-03-19 18:59:37 -0700

    feat: displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as sql
+
+    try:
+        db = sql.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    except Exception:
+        print("Can not connect to the database")
+        exit(0)
+
+    sql_querry = argv[4]
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = {:s} \
+                    ORDER BY id ASC;".format(sql_querry))
+
+    result_query = cursor.fetchall()
+
+    for row in result_query:
+        print(row)
+
+    cursor.close()
+    db.close()
