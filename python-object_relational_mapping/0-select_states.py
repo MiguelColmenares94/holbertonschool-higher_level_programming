5701482f5e085b0d27d9d2da015a40fee63183ad
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-03-19 18:19:28 -0700

    fix: typo when checking name global variable

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     from sys import argv
     import MySQLdb as mysql
 
