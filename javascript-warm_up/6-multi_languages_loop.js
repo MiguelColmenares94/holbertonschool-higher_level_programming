6f51f02a87e508be647fade1c13c375f76aaf0d7
MiguelColmenares94 <miguelog.alejandro@gmail.com>
2023-04-23 10:44:44 -0700

    feat: iterate over an array of strings and print index

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let counter;
+
+for (counter = 0; counter < array.length; counter++) {
+  console.log(array[counter]);
+}
