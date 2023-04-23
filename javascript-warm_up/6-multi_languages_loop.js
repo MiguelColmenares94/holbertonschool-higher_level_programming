ff87a19fbc8efd82af5d00781eb7662a6956bc94
MiguelColmenares94 <miguelog.alejandro@gmail.com>
2023-04-23 10:46:10 -0700

    fix: a typo on index array according to task

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let counter;
 
 for (counter = 0; counter < array.length; counter++) {
