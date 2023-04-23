558c0843dbf0d316dec19f6b9b148e0060e96f3a
MiguelColmenares94 <miguelog.alejandro@gmail.com>
2023-04-23 12:46:34 -0700

    feat: print a square depeding on input if is int

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let counterLine;
+let counterSize;
+
+if (Number.isNaN(Number(process.argv[2]))) {
+  console.log('Missing size');
+} else {
+  for (counterSize = 0; counterSize < process.argv[2]; counterSize++) {
+    for (counterLine = 0; counterLine < process.argv[2]; counterLine++) {
+      process.stdout.write('#');
+    }
+    console.log();
+  }
+}
