1e2a1cedeb7817640e03d1f3b34f4e4fd0d28906
MiguelColmenares94 <miguelog.alejandro@gmail.com>
2023-04-26 15:55:43 -0700

    fix: delete the array and just set the right path to required file

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-const filenames = ['./100-data', './data_0', './data_1', './data_2', './data_3'];
 
-for (const filename of filenames) {
-  const { list } = require(filename);
-  console.log(list);
-  console.log(list.map((value, index) => value * index));
-}
+const { list } = require('./100-data');
 
+console.log(list);
+console.log(list.map((value, index) => value * index));
