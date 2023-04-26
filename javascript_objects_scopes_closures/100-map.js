0a744b240a60029cc1ea0ace4cd7070cb098914f
MiguelColmenares94 <miguelog.alejandro@gmail.com>
2023-04-26 15:49:46 -0700

    fix: add an array of the files that will test the code

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-const { list } = require('./test/100-data');
-console.log(list);
-console.log(list.map((value, index) => value * index));
+const filenames = ['./100-data', './data_0', './data_1', './data_2', './data_3'];
+
+for (const filename of filenames) {
+  const { list } = require(filename);
+  console.log(list);
+  console.log(list.map((value, index) => value * index));
+}
+
