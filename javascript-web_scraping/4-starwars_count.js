fd8f2fc1d8dab1e85d4a0b287b59f5f10043b311
MiguelColmenares94 <miguelog.alejandro@gmail.com>
2023-04-29 18:07:43 -0700

    fix: add template string to searched url

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,16 +6,18 @@ request(process.argv[2], function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
     let count = 0;
+    let characterId = 18;
 
-    /* Another way to do it:
-     * const count = data.results.characters.filter(url => url.includes('/18/')).length;
-     */
+    /*  Another way to do it: 
+    *const count = data.results.characters.filter(url => url.includes('/18/')).length;
+    */
 
     data.results.forEach(function (movie) {
-      if (movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+     if (movie.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
         count++;
-      }
-    });
+       }
+     });
+     
 
     console.log(count);
   } else {
